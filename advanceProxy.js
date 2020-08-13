e988b80769e007091f1796a8a66e1e7e33964bdd
srkwritez <42516462+srkwritez@users.noreply.github.com>
2020-08-13 11:09:29 +0530

    Update advanceProxy.js

--- advanceProxy.js
+++ advanceProxy.js
@@ -1,7 +1,7 @@
 function FindProxyForURL(url, host) {
 
     // use proxy for specific domains
-    if (shExpMatch(host, "vrf-gl-sites.kao.wcm-ecm*"))
+    if (shExpMatch(host, "vrf-gl-sites.kao.wcm-ecm*|vrf-gl-public.kao.wcm-ecm.com*"))
         return "PROXY 10.22.63.22:3000";
 
     // by default use no proxy
